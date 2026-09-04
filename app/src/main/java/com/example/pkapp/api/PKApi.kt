5d5b173aac706c529caea7fe62403fa4f9da1bf7
y-sase <y-sase@esol.co.jp>
2026-09-04 09:56:33 +0900

    package名の変更、改行修正を行った

--- app/src/main/java/com/example/pkapp/api/PKApi.kt
+++ app/src/main/java/com/example/pkapp/api/PKApi.kt
@@ -3,16 +3,13 @@ package com.example.pkapp.api
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-
-
-
 interface PKApi {
 
     @GET("pokemon")
     suspend fun getPokemonList(
-        @Query("limit") limit: Int = 1351,
-        @Query("offset") offset: Int = 0
+        @Query("limit") limit: Int = 1351, @Query("offset") offset: Int = 0
     ): PokemonListResponse
+
     @GET("pokemon/{id}")
     suspend fun getPokemonDetail(
         @Path("id") id: Int
